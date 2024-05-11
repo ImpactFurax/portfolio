98c3dc2ced51af3c75be788ea3350184e2db8e92
ImpactFurax <bortjean@yahoo.fr>
2024-05-11 13:52:07 +0200

    fix about me

--- components/shared/AboutMe.tsx
+++ components/shared/AboutMe.tsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         <div className='relative size-48'>
           <Image
             src="/assets/images/profile-picture.png"
-            alt='Profile Picture'
+            alt="Profile Picture"
             fill
             className='object-cover rounded-full'
           />
